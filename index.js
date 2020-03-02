6b792151f9ed2773474c7d6c193d500837001078
Brandon Weaver <a7xncob@gmail.com>
2020-03-02 20:27:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ class Triangle extends Polygon {
 
 class Square extends Polygon {
   get isValid() {
-    return this._sides.reduce((memo, side) => + side, 0) === this._sides[0] * 4;
+    return this._sides.reduce((memo, side) => memo + side, 0) === this._sides[0] * 4;
   }
 }
